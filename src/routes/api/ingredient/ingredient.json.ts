7b370b02b781e527cb1f1c776deb23d9b2cd51f8
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2022-07-03 03:59:09 +0700

    Add: Static Images

--- src/routes/api/ingredient/ingredient.json.ts
+++ src/routes/api/ingredient/ingredient.json.ts
@@ -4,195 +4,233 @@ import Ingredient from '$lib/db/ingredients';
 const ALL_INGREDIENTS = [
 	{
 		name: 'Singha',
-		category: 'Beer'
+		category: 'Beer',
+		image_path: '/Beer/Singha.jpg'
 	},
 	{
 		name: 'Chang',
-		category: 'Beer'
+		category: 'Beer',
+		image_path: '/Beer/Chang.png'
 	},
 	{
 		name: 'Heineken',
-		category: 'Beer'
+		category: 'Beer',
+		image_path: '/Beer/Heineken.png'
 	},
 	{
 		name: 'Leo',
-		category: 'Beer'
+		category: 'Beer',
+		image_path: '/Beer/Leo.jpg'
 	},
 	{
 		name: "Gilbey's",
-		category: 'Gin'
+		category: 'Gin',
+		image_path: '/Gin/Gilbey.jpg'
 	},
 	{
 		name: 'Bombay Sapphire',
-		category: 'Gin'
+		category: 'Gin',
+		image_path: '/Gin/Bombay.jpg'
 	},
 	{
 		name: 'Regency',
-		category: 'Brandy'
+		category: 'Brandy',
+		image_path: '/Brandy/Regency.png'
 	},
 	{
 		name: 'Meridian',
-		category: 'Brandy'
+		category: 'Brandy',
+		image_path: '/Brandy/Meridian.jpg'
 	},
 	{
 		name: 'Johnnie Walker',
-		category: 'Whisky'
+		category: 'Whisky',
+		image_path: '/Whisky/Johnnie_Walker.png'
 	},
 	{
 		name: 'Bacardi',
-		category: 'Rum'
+		category: 'Rum',
+		image_path: '/RUM/Bacardi.jpg'
 	},
 	{
 		name: 'Mekhong',
-		category: 'Rum'
+		category: 'Rum',
+		image_path: '/RUM/Mekhong.jpeg'
 	},
 	{
 		name: 'Jose Cuervo Especial',
-		category: 'Tequila'
+		category: 'Tequila',
+		image_path: '/Tequila/Jose_Cuervo_Especial.jpg'
 	},
 	{
 		name: 'Sierra',
-		category: 'Tequila'
+		category: 'Tequila',
+		image_path: '/Tequila/Sierra.jpg'
 	},
 	{
 		name: 'Baczewski',
-		category: 'Vodka'
+		category: 'Vodka',
+		image_path: '/Vodka/Baczewski.jpg'
 	},
 	{
 		name: 'Kulov',
-		category: 'Vodka'
+		category: 'Vodka',
+		image_path: '/Vodka/Kulov.jpg'
 	},
 	{
 		name: 'Absolut',
-		category: 'Vodka'
+		category: 'Vodka',
+		image_path: '/Vodka/Absolut.jpg'
 	},
 	{
 		name: 'Gilbey’s 1857',
-		category: 'Vodka'
+		category: 'Vodka',
+		image_path: '/Vodka/Gilbey_1857.jpg'
 	},
 	{
 		name: 'Baileys',
-		category: 'Liqueur'
+		category: 'Liqueur',
+		image_path: '/Liqueur/Baileys.jpg'
 	},
 	{
 		name: 'Carolans',
-		category: 'Liqueur'
+		category: 'Liqueur',
+		image_path: '/Liqueur/Carolans.jpg'
 	},
 	{
 		name: 'Romana Sambvca',
-		category: 'Liqueur'
+		category: 'Liqueur',
+		image_path: '/Liqueur/Romana_Sambvca.png'
 	},
 	{
 		name: 'Good Day',
-		category: 'Soju'
+		category: 'Soju',
+		image_path: '/Soju/Good_Day.png'
 	},
 	{
 		name: 'Jinro',
-		category: 'Soju'
+		category: 'Soju',
+		image_path: '/Soju/Jinro.webp'
 	},
 	{
 		name: 'The Nanda',
-		category: 'Soju'
+		category: 'Soju',
+		image_path: '/Soju/The_Nanda.png'
 	},
 	{
 		name: 'Chum Churum',
-		category: 'Soju'
+		category: 'Soju',
+		image_path: '/Soju/Chum_Churum.png'
 	},
 	{
 		name: 'Charm',
-		category: 'Soju'
+		category: 'Soju',
+		image_path: '/Soju/Charm.png'
 	},
 	{
 		name: 'Makkolli',
-		category: 'Misc'
+		category: 'Misc',
+		image_path: '/Makkolli.jpg'
 	},
 	{
 		name: 'Apple Juice',
-		category: 'Fruit_Juice'
+		category: 'Fruit_Juice',
+		image_path: '/Juice/Apple_Juice.jpg'
 	},
 	{
 		name: 'Orange Juice',
-		category: 'Fruit_Juice'
-	},
-	{
-		name: 'Watermelon Juice',
-		category: 'Fruit_Juice'
+		category: 'Fruit_Juice',
+		image_path: '/Juice/Orange_Juice.jpg'
 	},
 	{
 		name: 'Grape Juice',
-		category: 'Fruit_Juice'
+		category: 'Fruit_Juice',
+		image_path: '/Juice/Grape_Juice.jpg'
 	},
 	{
 		name: 'Lemon Juice',
-		category: 'Fruit_Juice'
+		category: 'Fruit_Juice',
+		image_path: '/Juice/Lemon_Juice.jpg'
 	},
 	{
 		name: 'Strawberry Juice',
-		category: 'Fruit_Juice'
+		category: 'Fruit_Juice',
+		image_path: '/Juice/Strawberry_Juice.jpg'
 	},
 	{
 		name: 'Weed Juice',
-		category: 'Fruit_Juice'
+		category: 'Fruit_Juice',
+		image_path: '/Juice/Weed_Juice.jpg'
 	},
 	{
 		name: 'Vanilla Syrup',
-		category: 'Cocktail_Sweeteners'
+		category: 'Cocktail_Sweeteners',
+		image_path: '/any/Vanilla_Syrup.jpg'
 	},
 	{
 		name: 'Lemon Syrup',
-		category: 'Cocktail_Sweeteners'
+		category: 'Cocktail_Sweeteners',
+		image_path: '/any/Lemon_Syrup.webp'
 	},
 	{
 		name: 'Blue Hawaii Syrup',
-		category: 'Cocktail_Sweeteners'
+		category: 'Cocktail_Sweeteners',
+		image_path: '/any/Blue_Hawaii_Syrup.jpg'
 	},
 	{
 		name: 'Maple Syrup',
-		category: 'Cocktail_Sweeteners'
+		category: 'Cocktail_Sweeteners',
+		image_path: '/any/Maple_Syrup.jpg'
 	},
 	{
 		name: 'Sugar Syrup',
-		category: 'Cocktail_Sweeteners'
+		category: 'Cocktail_Sweeteners',
+		image_path: '/any/Sugar_Syrup.jpg'
 	},
 	{
 		name: 'Honey Syrup',
-		category: 'Cocktail_Sweeteners'
+		category: 'Cocktail_Sweeteners',
+		image_path: '/any/Honey_Syrup.jpg'
 	},
 	{
 		name: 'Coke',
-		category: 'Softdrink'
+		category: 'Softdrink',
+		image_path: '/any/Coke.jpg'
 	},
 	{
 		name: 'Pepsi',
-		category: 'Softdrink'
+		category: 'Softdrink',
+		image_path: '/any/Pepsi.jpg'
 	},
 	{
 		name: 'Soda',
-		category: 'Softdrink'
+		category: 'Softdrink',
+		image_path: '/any/Soda.jpg'
 	},
 	{
 		name: 'Sprite',
-		category: 'Softdrink'
+		category: 'Softdrink',
+		image_path: '/any/Sprite.png'
 	},
 	{
 		name: 'Jelly',
-		category: 'Misc'
+		category: 'Misc',
+		image_path: '/any/Jelly.jpg'
 	},
 	{
 		name: 'Boba',
-		category: 'Misc'
+		category: 'Misc',
+		image_path: '/any/Boba.jpg'
 	},
 	{
 		name: 'Blend 285',
-		category: 'Misc'
+		category: 'Misc',
+		image_path: 'Blend_285.jpg'
 	},
 	{
-		name: 'Wort',
-		category: 'Misc'
-	},
-	{
-		name: 'rice whiskya',
-		category: 'Misc'
+		name: 'Rice Whisky',
+		category: 'Misc',
+		image_path: 'Rice_Whisky.jpg'
 	}
 ];
 
