f4bd1e68f06131f2d5f2d57fefd729e43350ecbb
Supratouch <supratouchgussii@gmail.com>
2022-07-03 04:18:32 +0700

    pull asap

--- src/routes/api/post/index.json.ts
+++ src/routes/api/post/index.json.ts
@@ -1,6 +1,6 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import { customAlphabet } from 'nanoid';
-import Post from '$lib/db/post';
+import Post, { type IPost } from '$lib/db/post';
 
 const nanoid = customAlphabet('1234567890abcdef', 10);
 
@@ -14,45 +14,49 @@ export const get: RequestHandler = async () => {
 	};
 };
 
-export const post: RequestHandler = async ({ request }) => {
-	const body = await request.json();
-	const { post_name, ingredients, post_by } = body;
+export const post: RequestHandler = async ({ request }: { request: Request }) => {
+	try {
+		const body = await request.json();
+		const { post_name, ingredients, post_by } = body;
 
-	if (!ingredients && !post_by) {
-		return {
-			status: 400,
-			body: {
-				message: 'Missing some required information.'
-			}
-		};
-	}
+		if (!ingredients && !post_by) {
+			return {
+				status: 400,
+				body: {
+					message: 'Missing some required information.'
+				}
+			};
+		}
 
-	let payload = { created_at: new Date(), ...body };
-	let ref_id = nanoid(8);
+		let payload = { created_at: new Date(), ...body };
+		let ref_id = nanoid(8);
 
-	while (await Post.findOne({ ref_id: ref_id })) {
-		ref_id = nanoid(8);
-	}
+		const existingPost = await Post.findOne<IPost>({
+			ref_id
+		});
 
-	if (!post_name) {
-		payload = { ref_id: ref_id, post_name: ref_id, ...payload };
-	} else {
-		payload = { ref_id: ref_id, ...payload };
-	}
-
-	const post = await Post.create(payload);
-	if (post) {
-		return {
-			status: 200,
-			body: {
-				data: post
-			}
-		};
-	} else {
+		if (existingPost) {
+			return {
+				status: 200,
+				body: {
+					data: JSON.stringify(existingPost)
+				}
+			};
+		} else {
+			const post = await Post.create(payload);
+			return {
+				status: 200,
+				body: {
+					data: JSON.stringify(post)
+				}
+			};
+		}
+	} catch (e) {
+		console.log(e);
 		return {
 			status: 500,
 			body: {
-				message: 'Error!'
+				data: JSON.stringify(e)
 			}
 		};
 	}
