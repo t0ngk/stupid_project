9c1c22238d39d4faa1229692b4eff4cc738e95f4
C0M3TZZ <39180519+C0M3TZZ@users.noreply.github.com>
2022-07-02 13:12:11 +0700

    Add: Svelte Fa

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,12 @@
+<script lang="ts">
+  import Fa from 'svelte-fa/src/fa.svelte'
+  import { faFlag } from '@fortawesome/free-solid-svg-icons'
+</script>
+
 <h1 class="text-3xl font-bold underline">
   Stupid Project
 </h1>
 <div class="btn">
   This Btn From DaisyUI
 </div>
+<Fa icon={faFlag}></Fa>
