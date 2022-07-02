c40ff75a53782c0d7d7fff676688a65a52a46468
Supratouch <supratouchgussii@gmail.com>
2022-07-02 21:19:41 +0700

    schema update

--- src/lib/db/ingredients.ts
+++ src/lib/db/ingredients.ts
@@ -2,7 +2,24 @@ import mongoose from 'mongoose';
 
 export const ingredientsSchema = new mongoose.Schema({
 	name: String,
-	category_id: Number,
+	category_id: {
+		type: String,
+		enum: [
+			'Beer',
+			'Gin',
+			'Brandy',
+			'Whisky',
+			'Rum',
+			'Tequila',
+			'Vodka',
+			'Liqueur',
+			'Soju',
+			'Fruit_Juice',
+			'Cocktail_Sweeteners',
+			'Softdrink',
+			'Misc'
+		]
+	},
 	is_allowed: Boolean
 });
 
