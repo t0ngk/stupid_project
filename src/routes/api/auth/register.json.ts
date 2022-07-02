35d3a225f1f8572885fbf83873289d50a16454d3
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2022-07-02 13:42:01 +0700

    Fix: Unexpected JSON Input

--- src/routes/api/auth/register.json.ts
+++ src/routes/api/auth/register.json.ts
@@ -2,18 +2,6 @@ import type { RequestHandler, RequestEvent } from '@sveltejs/kit';
 
 export const post: RequestHandler = async (event: RequestEvent) => {
 	const body = await event.request.json();
-	console.log(body);
-	return {
-		status: 200,
-		body: {
-			body
-		}
-	};
-};
-
-export const patch: RequestHandler = async ({ request }) => {
-	const body = await request.json();
-	console.log(body);
 	return {
 		status: 200,
 		body: {
