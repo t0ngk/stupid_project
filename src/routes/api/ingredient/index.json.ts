368c63f2b12a5a72527cafbafcca2ae93e1e474f
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2022-07-02 22:09:22 +0700

    Add: All Ingredients to database and Fix import error

---
+++ src/routes/api/ingredient/index.json.ts
@@ -0,0 +1,34 @@
+import type { RequestHandler } from '@sveltejs/kit';
+import Ingredient from '$lib/db/ingredients';
+
+export const post: RequestHandler = async ({ request }) => {
+	const body = await request.json();
+	const { name, category_id, is_allowed } = body;
+
+	if (!name && !category_id && !is_allowed) {
+		return {
+			status: 400,
+			body: {
+				message: 'Missing some required information.'
+			}
+		};
+	}
+
+	const ingredient = await Ingredient.create(body);
+
+	if (!ingredient) {
+		return {
+			status: 500,
+			body: {
+				message: 'Error!'
+			}
+		};
+	}
+
+	return {
+		status: 200,
+		body: {
+			data: ingredient
+		}
+	};
+};
