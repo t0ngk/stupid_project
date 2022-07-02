79f4f13524429683eda3598540c035dfb1a03754
C0M3TZZ <39180519+C0M3TZZ@users.noreply.github.com>
2022-07-03 03:06:47 +0700

    fix: vercel from PNPM?

--- src/routes/age/index.svelte
+++ src/routes/age/index.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import * as dayjs from 'dayjs';
-	import * as relativeTime from 'dayjs/plugin/relativeTime';
+	import dayjs from 'dayjs';
+	import relativeTime from 'dayjs/plugin/relativeTime.js';
 	import { onMount } from 'svelte';
 
 	let age:any = null;
