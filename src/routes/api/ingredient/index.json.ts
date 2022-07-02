18323afc7afd072c743f28eec99fc13546bc1d09
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2022-07-02 22:12:39 +0700

    Add: GET All Ingredients Route

--- src/routes/api/ingredient/index.json.ts
+++ src/routes/api/ingredient/index.json.ts
@@ -1,6 +1,25 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import Ingredient from '$lib/db/ingredients';
 
+export const get: RequestHandler = async () => {
+	const ingredients = await Ingredient.find({});
+	if (ingredients) {
+		return {
+			status: 200,
+			body: {
+				data: ingredients
+			}
+		};
+	} else {
+		return {
+			status: 500,
+			body: {
+				message: 'Error!'
+			}
+		};
+	}
+};
+
 export const post: RequestHandler = async ({ request }) => {
 	const body = await request.json();
 	const { name, category_id, is_allowed } = body;
