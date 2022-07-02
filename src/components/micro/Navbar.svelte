e78f3ecd0431988ced1f77c893972a7f33671ae2
Supratouch <supratouchgussii@gmail.com>
2022-07-02 21:53:18 +0700

    nav

---
+++ src/components/micro/Navbar.svelte
@@ -0,0 +1,18 @@
+<script>
+
+import { goto } from "$app/navigation";
+
+</script>
+<div class="z-20 px-8 h-12 bg-black flex justify-between items-center font-semibold text-white fixed left-0 right-0 top-0">
+    <h3 on:click={() => goto('/home')} class="cursor-pointer">
+        Taoloi 
+    </h3>
+    <div class="flex justify-center items-center gap-x-8 text-sm">
+        <h3 on:click={() => goto('/mix')} class="cursor-pointer">
+            Mix
+        </h3>
+        <h3 on:click={() => goto('/browse')} class="cursor-pointer">
+            Browse
+        </h3>
+    </div>
+</div>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,23 @@
 <script lang="ts">
+import { page } from "$app/stores";
+import { onMount } from "svelte";
+
   import "../app.css";
+import Navbar from "../components/micro/Navbar.svelte";
+
+  let enableNavbar : boolean = false
+
+  onMount( () => {
+    if (['home', 'mix', 'browse', 'view', 'comment'].includes(window.location.pathname.replaceAll('/', ''))) enableNavbar = true
+  })
+
 </script>
 
+{
+  #if enableNavbar
+}
+<Navbar/>
+{
+  /if
+}
 <slot />
